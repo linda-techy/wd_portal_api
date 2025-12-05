1c04fe8a92c6e9cd00ba9ac3ab1638ee6de76c40
Linda <linda.tech24@gmail.com>
2025-12-06 01:23:04 +0530

    feat: add ProjectMember entity to manage project assignments and roles for portal and customer users.

--- src/main/java/com/wd/api/model/ProjectMember.java
+++ src/main/java/com/wd/api/model/ProjectMember.java
@@ -20,41 +20,7 @@ public class ProjectMember {
     private PortalUser portalUser;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "customer_user_id") // Assuming this column name based on pattern, though user didn't explicitly
-                                           // show it in screenshot, but mentioned "customer_users" table. Wait, the
-                                           // screenshot shows "customer_id" and "portal_user_id". "customer_id" usually
-                                           // refers to the Customer entity, not CustomerUser. But the user said
-                                           // "dropdown of team members should load from table both customer_users and
-                                           // portal_users".
-    // Let's look at the screenshot again.
-    // Screenshot 1: project_members table has: customer_id (int8), project_id
-    // (int8), role_in_project (varchar), portal_user_id (int8).
-    // It seems "customer_id" might be referring to "Customer" entity or
-    // "CustomerUser"?
-    // The user said "load from table both customer_users and portal_users and add
-    // to this project_members table".
-    // If "customer_id" refers to "CustomerUser", then I should map it to
-    // CustomerUser entity.
-    // However, usually "customer_id" refers to the Customer (organization).
-    // Let's check CustomerUser.java again. It has @Table(name = "customer_users").
-    // Let's check Customer.java if possible.
-    // But given the user request "add to this project_members table", and the
-    // columns are `customer_id` and `portal_user_id`.
-    // I will assume `customer_id` in `project_members` is intended to link to
-    // `CustomerUser` based on the context of "team members".
-    // OR, maybe `customer_id` is for the Customer entity, and there is no column
-    // for `CustomerUser` yet?
-    // The user said "add to this project_members table".
-    // If I look at the screenshot, it has `customer_id`.
-    // If I look at `CustomerUser` entity, it is a user.
-    // If I look at `Customer` entity, it is likely a company/client.
-    // The user wants to assign "team members".
-    // "Team members" usually implies people.
-    // So `CustomerUser` makes sense.
-    // But the column name is `customer_id`.
-    // I will map `customer_id` to `CustomerUser` for now, but I should be careful.
-    // Actually, looking at the screenshot, `customer_id` is `int8`.
-    // I'll assume it maps to `CustomerUser`.
+    @JoinColumn(name = "customer_user_id")
 
     private CustomerUser customerUser;
 
