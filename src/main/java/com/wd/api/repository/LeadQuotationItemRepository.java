d1ec271855278f1f49066841f0981a02987a8727
Linda <linda.tech24@gmail.com>
2026-05-02 16:24:30 +0530

    refactor(legacy): delete 8 quotation entities + 8 repositories + 18 DTOs

--- src/main/java/com/wd/api/repository/LeadQuotationItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wd.api.repository;
-
-import com.wd.api.model.LeadQuotationItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LeadQuotationItemRepository extends JpaRepository<LeadQuotationItem, Long> {
-}
